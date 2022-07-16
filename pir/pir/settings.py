43ebcd95e693d48678da251ddf39dfec3312e6d9
= <=>
2022-07-16 16:37:34 +0500

    final code

--- pir/pir/settings.py
+++ pir/pir/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kzagz312*aad^w_(1(g%pg@pml%bpqf4%g)zvc*9fdr&*-=vh2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"), ]
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles"),
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 # Default primary key field type
